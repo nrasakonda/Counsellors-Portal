b2a48c65c3afe7c3a967a48e930f1c642b42ae84
nrasakonda <nrasakonda@gmail.com>
2025-08-27 23:47:31 +0530

    Update EnquiryService.java
    Changed

--- src/main/java/in/java/service/EnquiryService.java
+++ src/main/java/in/java/service/EnquiryService.java
@@ -2,26 +2,13 @@ package in.java.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
-import in.java.dto.EnquiryDto;
-import in.java.dto.EnquiryFilterRequestDto;
-import in.java.entities.Course;
 import in.java.entities.Enquiry;
 
-@Service
 public interface EnquiryService {
 
-	public boolean insertEnquiry(EnquiryDto enquiryDto, Integer counsellorId);
+    // Save enquiry
+    boolean insertEnquiry(Enquiry enquiry);
 
-	public boolean updateEnquiry(EnquiryDto enquiryDto);
-
-	public List<Enquiry> getAllEnquiry(Integer counsellorId);
-
-	public List<Enquiry> getEnquiryByFilter(EnquiryFilterRequestDto filter, Integer counsellorId);
-
-	public Enquiry getEnquiry(Integer enquiryId);
-
-	public List<Course> getCourses();
-
+    // Get all enquiries of a counsellor
+    List<Enquiry> getAllEnquiry(Integer counsellorId);
 }
